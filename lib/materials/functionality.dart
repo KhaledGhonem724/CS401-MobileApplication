ca6a4e43170da82354389cd1b247cb8f09e29095
KhaledGhonem724 <kforex2000@gmail.com>
2023-05-15 10:48:24 +0300

    fixed sime error for marco : pubspect.yaml SDK version

--- lib/materials/functionality.dart
+++ lib/materials/functionality.dart
@@ -1,3 +1,30 @@
+import 'dart:ffi';
+
+timeData toTime(int mlsec) {
+  int sec = (mlsec / 1000).toInt();
+  int min = (sec / 60).toInt();
+  sec %= 60;
+  int hr = (min / 60).toInt();
+  min %= 60;
+  return timeData(hr, min, sec);
+}
+
+class timeData {
+  int hr = 0, min = 0, sec = 0;
+  timeData(int hr, int min, int sec) {
+    this.hr = hr;
+    this.min = min;
+    this.sec = sec;
+  }
+}
+
 class sessionData {
-  sessionData(String taskName) {}
+  String taskName = "";
+  timeData sessionTime = timeData(0, 0, 0);
+  DateTime sessionDate = DateTime.now();
+  sessionData(String taskName, timeData sessionTime, DateTime sessionDate) {
+    this.taskName = taskName;
+    this.sessionTime = sessionTime;
+    this.sessionDate = sessionDate;
+  }
 }
